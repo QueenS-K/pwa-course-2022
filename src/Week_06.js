3e66c03b8a056be04f855179d8e2a94944902f3a
QueenS-K <jukrapat.2010@hotmail.com>
2022-02-25 19:49:14 +0700

    Week_06_Complete

---
+++ src/Week_06.js
@@ -0,0 +1,14 @@
+import './css/Week_06.css';
+
+function Week_06() {
+    return (
+        <div> 
+            <div className="ball">A</div>
+            <div className="ball">B</div>
+            <div className="ball">C</div>
+        </div>
+
+    )
+}
+
+export default Week_06;
