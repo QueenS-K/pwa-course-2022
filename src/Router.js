ef0463790727ed27ee423f3ae7421878a6aefea1
QueenS-K <jukrapat.2010@hotmail.com>
2022-03-04 23:13:33 +0700

    Week_07_Complete

--- src/Router.js
+++ src/Router.js
@@ -1,19 +1,23 @@
-import { BrowserRouter, Routes ,Route } from 'react-router-dom';
-import App from './App.js';
-import Profile from './Profile.js';
-import Week_06 from './Week_06.js';
+import {
+  BrowserRouter,
+  Routes,
+  Route
+} from "react-router-dom";
+import Chat from "./Chat";
+import Profile from "./Profile";
+import FriendList from "./FriendList";
 
 function Router() {
-    return (
-        <BrowserRouter>
-            <Routes>
-                <Route path="/" element={ <App/>} />
-                <Route path="/profile" element={ <Profile/>} />
-                <Route path="/Week_06" element={ <Week_06/>} />
-            </Routes>
-        </BrowserRouter>
-
-    )
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/chat" element={<Chat />}/>
+        <Route path="/profile" element={<Profile />}/>
+        <Route path="/friend-list" element={<FriendList />}/>
+        <Route path="/" element={<Chat />}/>
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default Router;
